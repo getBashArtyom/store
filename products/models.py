d7761ac20153aed917985ad33c5a67e27aa72c0a
artyombash <artyombashsts@gmail.com>
2023-03-23 00:37:40 +0300

    basket add

--- products/models.py
+++ products/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+
+from users.models import User
 # Create your models here.
 
 class ProductCategory(models.Model):
@@ -12,7 +14,7 @@ class ProductCategory(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
-    descripton = models.TextField(null=True)
+    description = models.TextField(null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_image')
@@ -20,3 +22,23 @@ class Product(models.Model):
 
     def __str__(self):
         return f'Продукт: {self.name} | Категория: {self.category.name}'
+
+
+class BasketQuerySet(models.QuerySet):
+    def total_sum(self):
+        return sum(basket.sum() for basket in self)
+
+    def total_quantity(self):
+        return sum(basket.quantity for basket in self)
+class Basket(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = BasketQuerySet.as_manager()
+    def __str__(self):
+        return f'Корзина: {self.user.username} | Продукт: {self.product.name}'
+
+    def sum(self):
+        return self.product.price * self.quantity
--- users/views.py
+++ users/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from users.models import User
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
 
 from users.forms import UserLoginForm, UserRegistratonForm, UserProfileForm
-
+from products.models import Basket
 
 def login(request):
     if request.method == 'POST':
@@ -34,7 +36,7 @@ def registration(request):
         form = UserRegistratonForm()
     context = {'form': form}
     return render(request, 'users/registration.html', context)
-
+@login_required
 def profile(request):
     if request.method == 'POST':
         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
@@ -43,7 +45,11 @@ def profile(request):
             return HttpResponseRedirect(reverse('users:profile'))
     else:
         form = UserProfileForm(instance=request.user)
-    context = {'title': 'Профиль', 'form': form}
+        baskets = Basket.objects.filter(user=request.user)
+    context = {'title': 'Профиль',
+               'form': form,
+               'baskets': baskets,
+               }
     return render(request, 'users/profile.html', context)
 
 def logout(requst):
