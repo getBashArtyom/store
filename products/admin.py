2191b496a04483848fc5dd18bb88bcaaf85a201f
artyombash <artyombashsts@gmail.com>
2023-03-23 12:48:54 +0300

    admin refactor + paginator add

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 
-from products.models import ProductCategory, Product
+from products.models import ProductCategory, Product, Basket
 
-admin. site.register(Product)
-admin. site.register(ProductCategory)
+admin.site.register(ProductCategory)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'description', ('price', 'quantity'), 'image', 'category',)
+    readonly_fields = ('description',)
+    search_fields = ('name',)
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity')
+    readonly_fields = ('created_timestamp', 'created_timestamp',)
+    extra = 0
--- products/models.py
+++ products/models.py
@@ -6,7 +6,11 @@ from users.models import User
 
 class ProductCategory(models.Model):
     name = models.CharField(max_length=128)
-    descripton = models.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+    class Meta():
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
@@ -20,6 +24,9 @@ class Product(models.Model):
     image = models.ImageField(upload_to='products_image')
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
 
+    class Meta():
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
     def __str__(self):
         return f'Продукт: {self.name} | Категория: {self.category.name}'
 
